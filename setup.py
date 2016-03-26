b12dc970bb82e91590e5d109d3b04753012b9578
Wilbert Berendsen <wbsoft@xs4all.nl>
2016-03-26 07:12:18 +0100

    fix #7

--- setup.py
+++ setup.py
@@ -288,7 +288,7 @@ class build_ext(build_ext_base):
 
 if platform.system() == 'Windows':
    # Enforce libraries to link against on Windows
-   ext_args['libraries'] = ['poppler-qt5', 'QtCore5', 'QtGui5', 'QtXml5']
+   ext_args['libraries'] = ['poppler-qt5', 'Qt5Core', 'Qt5Gui', 'Qt5Xml']
    
    class bdist_support():
        def __find_poppler_dll(self):
